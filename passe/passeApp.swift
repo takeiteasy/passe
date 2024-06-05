6d032a2a4492a43fe42b07cd0f9e55a6712a63c5
George Watson <gigolo@hotmail.co.uk>
2024-06-05 21:36:25 +0100

    initial commit

---
+++ passe/passeApp.swift
@@ -0,0 +1,17 @@
+//
+//  passeApp.swift
+//  passe
+//
+//  Created by George Watson on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct passeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
